035c0757f1896b2e72f389e3911b664295de8e59
Mike Dilger <mike@optcomp.nz>
2016-12-01 14:25:30 +1300

    Error: handle MailstromStorageError (as a String for ease)

--- src/error.rs
+++ src/error.rs
@@ -3,11 +3,13 @@ use std::convert::From;
 use std::sync::mpsc::SendError;
 use email_format::rfc5322::ParseError;
 use worker::Message;
+use storage::MailstromStorageError;
 
 #[derive(Debug)]
 pub enum Error {
     Send(SendError<Message>),
     EmailParser(ParseError),
+    Storage(String),
 }
 
 impl From<SendError<Message>> for Error {
@@ -21,5 +23,12 @@ impl From<ParseError> for Error {
     fn from(e: ParseError) -> Error
     {
         Error::EmailParser(e)
+    }
+}
+
+impl<S: MailstromStorageError> From<S> for Error {
+    fn from(e: S) -> Error
+    {
+        Error::Storage(format!("{}", e))
     }
 }
