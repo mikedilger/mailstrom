9a91a73fa21638df9efd7c34c73c7097b3652b9b
Mike Dilger <mike@optcomp.nz>
2016-12-06 10:00:29 +1300

    Add an example

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,47 @@
+//! Mailstrom handles email delivery.
+//!
+//! You can use it as follows:
+//!
+//! ```no_run
+//! extern crate email_format;
+//! extern crate mailstrom;
+//!
+//! use email_format::Email;
+//! use mailstrom::{Mailstrom, Config, MemoryStorage};
+//!
+//! fn main() {
+//!     let mut email = Email::new(
+//!         "myself@mydomain.com",  // "From:"
+//!         "Wed, 05 Jan 2015 15:13:05 +1300" // "Date:"
+//!     ).unwrap();
+//!
+//!     email.set_bcc("myself@mydomain.com").unwrap();
+//!     email.set_sender("from_myself@mydomain.com").unwrap();
+//!     email.set_reply_to("My Mailer <no-reply@mydomain.com>").unwrap();
+//!     email.set_to("You <you@yourdomain.com>, AndYou <andyou@yourdomain.com>").unwrap();
+//!     email.set_cc("Our Friend <friend@frienddomain.com>").unwrap();
+//!     email.set_subject("Hello Friend").unwrap();
+//!     email.set_body("Good to hear from you.\r\n\
+//!                     I wish you the best.\r\n\
+//!                     \r\n\
+//!                     Your Friend").unwrap();
+//!
+//!     let mut mailstrom = Mailstrom::new(
+//!         Config {
+//!             helo_name: "my.host.domainname".to_owned(),
+//!         },
+//!         MemoryStorage::new());
+//!
+//!     let message_id = mailstrom.send_email(email).unwrap();
+//!
+//!     // Later on, after the worker thread has had time to process the request,
+//!     // you can check the status:
+//!
+//!     let status = mailstrom.query_status(&*message_id).unwrap();
+//!     println!("{:?}", status);
+//! }
+//! ```
+
 
 #![feature(integer_atomics)]
 #![feature(proc_macro)]
