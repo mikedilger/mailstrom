fd049d20d3b8080c4beeb31f8d0f6cf56633c25b
Mike Dilger <mike@optcomp.nz>
2016-12-08 14:56:58 +1300

    Remove test that we cannot run safely

--- src/tests.rs
+++ src/tests.rs
@@ -14,33 +14,3 @@ fn test_terminate() {
     ::std::thread::sleep(::std::time::Duration::from_millis(100));
     assert_eq!( mailstrom.worker_status(), WorkerStatus::Terminated );
 }
-
-/*
-// We cannot run such a test without actually sending mail to some SMTP server.
-// We will work out how to fake this in a future release
-#[test]
-fn test_submit_email() {
-
-    use email_format::Email;
-
-    let mut mailstrom = Mailstrom::new(Config { helo_name: "localhost".to_owned() },
-                                       MemoryStorage::new());
-
-    let mut email = Email::new(
-        "myself@mydomain.com",  // "From:"
-        "Wed, 05 Jan 2015 15:13:05 +1300" // "Date:"
-            ).unwrap();
-    email.set_bcc("myself@mydomain.com").unwrap();
-    email.set_sender("from_myself@mydomain.com").unwrap();
-    email.set_reply_to("My Mailer <no-reply@mydomain.com>").unwrap();
-    email.set_to("You <you@yourdomain.com>, AndYou <andyou@yourdomain.com>").unwrap();
-    email.set_cc("Our Friend <friend@frienddomain.com>").unwrap();
-    email.set_subject("Hello Friend").unwrap();
-    email.set_body("Good to hear from you.\r\n\
-                    I wish you the best.\r\n\
-                    \r\n\
-                    Your Friend").unwrap();
-
-    assert!( mailstrom.send_email(email).is_ok() );
-}
- */
