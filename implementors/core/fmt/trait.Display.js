42a5c684ada711580630d7a36c47ce8b073942c7
Mike Dilger <mike@optcomp.nz>
2016-12-14 10:41:14 +1300

    Updated for 0.3

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["mailstrom"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='mailstrom/error/enum.Error.html' title='mailstrom::error::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
